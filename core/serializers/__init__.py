19b251776bf0a32ffe1745db85a883c41d72b659
Haniero <haniero.iuchi@gmail.com>
2024-11-28 08:57:34 -0300

    Criando um serializador específico para a listagem de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -4,9 +4,10 @@ from .editora import EditoraSerializer
 from .autor import AutorSerializer
 from .livro import LivroSerializer, LivroListSerializer, LivroRetrieveSerializer
 from .compra import (
-    CompraSerializer,
+    CompraListSerializer,
     CompraCreateUpdateSerializer,
+    CompraSerializer,
+    ItensCompraCreateUpdateSerializer,
     ItensCompraListSerializer,
     ItensCompraSerializer,
-    CompraListSerializer,
 )
