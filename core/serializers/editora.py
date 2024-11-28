b5a0a0b85d572fc54f969886f8549d4105cb14b7
Haniero <haniero.iuchi@gmail.com>
2024-11-28 09:33:12 -0300

    Formatando dados antes de salvar

--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -1,8 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Livro
-...
+from core.models import Editora
+
+
 class EditoraSerializer(ModelSerializer):
     class Meta:
-        model = Livro
+        model = Editora
         fields = "__all__"
+
+    def validate_email(self, email):
+        return email.lower()
